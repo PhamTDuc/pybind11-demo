ab5eec31954f7fd86a944c71d1a9e6a99f2f1b6a
Pham Duc <phamduc0701@gmail.com>
2020-10-11 23:01:26 +0700

    + Add test_sample to test library

---
+++ test_sample.py
@@ -0,0 +1,8 @@
+import example
+print(example.add(2235, 5214))
+my_dog = example.Pet("Pluto", 7)
+print(my_dog.get_name())
+my_dog.get_hunger()
+my_dog.go_for_a_walk()
+my_dog.go_for_a_walk()
+print(my_dog.get_hunger())
